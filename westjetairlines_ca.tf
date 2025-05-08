2ebeb776b1d7832db913d03c6fc3fec9aa9bff75
Mark Strother <mark@pacific.ca>
2025-05-08 16:09:18 -0700

    Add zones

---
+++ westjetairlines_ca.tf
@@ -0,0 +1,14 @@
+module "westjetairlines_ca" {
+  source     = "./modules/dns_zone"
+  zone_name  = "westjetairlines.ca"
+  www_ip     = "1.2.3.5"
+  spf_record = "v=spf1 include:spf.protection.outlook.com -all"
+}
+
+resource "azurerm_dns_a_record" "westjetairlines_ca_api" {
+  name                = "api"
+  zone_name           = module.westjetairlines_ca.zone_name
+  resource_group_name = "rg-westjet-dns"
+  ttl                 = 300
+  records             = ["4.5.6.11"]
+}
