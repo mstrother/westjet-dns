36d0a48a76a0f8e6dcc295c59094808967b92e1e
Mark Strother <mark@pacific.ca>
2025-05-08 16:18:59 -0700

    Move outputs

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "zone_name" {
-  description = "The DNS zone name."
-  value       = azurerm_dns_zone.this.name
-}
-
-output "resource_group_name" {
-  description = "The DNS zone's resource group."
-  value       = azurerm_dns_zone.this.resource_group_name
-}
